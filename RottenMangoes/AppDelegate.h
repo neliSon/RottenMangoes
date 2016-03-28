6c3dd5e0c0ac39f8ce3307896e423bd24c542cc6
neliSun <nelson.chow91.nc@gmail.com>
2016-03-28 12:56:47 -0700

    Initial Commit

---
+++ RottenMangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenMangoes
+//
+//  Created by Nelson Chow on 2016-03-28.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenMangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangoes
+//
+//  Created by Nelson Chow on 2016-03-28.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
